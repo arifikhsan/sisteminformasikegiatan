416a097f907b27a9fdb7ce56a3c72688f4cc8bdc
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-07-19 16:57:12 +0700

    initial commit

---
+++ src/main/java/sh/now/arifikhsanudin/sisteminformasikegiatan/SistemInformasiKegiatanApplication.java
@@ -0,0 +1,13 @@
+package sh.now.arifikhsanudin.sisteminformasikegiatan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemInformasiKegiatanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemInformasiKegiatanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sh/now/arifikhsanudin/sisteminformasikegiatan/SistemInformasiKegiatanApplicationTests.java
@@ -0,0 +1,13 @@
+package sh.now.arifikhsanudin.sisteminformasikegiatan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemInformasiKegiatanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
