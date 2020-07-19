ff74e6a0be9d4942fc8b751a5c5eae8e5aea7a8b
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-07-19 17:10:02 +0700

    skeleton

---
+++ src/main/java/sh/now/arifikhsanudin/sisteminformasikegiatan/controller/TodoController.java
@@ -0,0 +1,15 @@
+package sh.now.arifikhsanudin.sisteminformasikegiatan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TodoController {
+
+    @GetMapping("/kegiatan")
+    public String greeting(Model model) {
+        return "kegiatan";
+    }
+}
